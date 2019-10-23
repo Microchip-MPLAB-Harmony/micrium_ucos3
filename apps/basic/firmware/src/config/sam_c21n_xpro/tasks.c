2ea25a46097977abe536dd7802c8419f69b3dfba
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-10-23 17:24:26 +0530

    [apps] Create a basic Micrium OS III application for sam_c21n_xpro configuration
    MH3-23513
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

---
+++ apps/basic/firmware/src/config/sam_c21n_xpro/tasks.c
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ System Tasks File
+
+  File Name:
+    tasks.c
+
+  Summary:
+    This file contains source code necessary to maintain system's polled tasks.
+
+  Description:
+    This file contains source code necessary to maintain system's polled tasks.
+    It implements the "SYS_Tasks" function that calls the individual "Tasks"
+    functions for all polled MPLAB Harmony modules in the system.
+
+  Remarks:
+    This file requires access to the systemObjects global data structure that
+    contains the object handles to all MPLAB Harmony module objects executing
+    polled in the system.  These handles are passed into the individual module
+    "Tasks" functions to identify the instance of the module to maintain.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "configuration.h"
+#include "definitions.h"
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: RTOS "Tasks" Routine
+// *****************************************************************************
+// *****************************************************************************
+/* Handle for the APP_Tasks. */
+OS_TCB  _APP_Tasks_TCB;
+CPU_STK _APP_TasksStk[512];
+
+void _APP_Tasks(  void *pvParameters  )
+{
+    OS_ERR os_err;
+    while(1)
+    {
+        APP_Tasks();
+        OSTimeDly(500 , OS_OPT_TIME_DLY, &os_err);
+    }
+}
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System "Tasks" Routine
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void SYS_Tasks ( void )
+
+  Remarks:
+    See prototype in system/common/sys_module.h.
+*/
+void SYS_Tasks ( void )
+{
+    OS_ERR os_err;
+    /* Maintain system services */
+    
+
+    /* Maintain Device Drivers */
+    
+
+    /* Maintain Middleware & Other Libraries */
+    
+
+    /* Maintain the application's state machine. */
+    
+    /* Create OS Thread for APP_Tasks. */
+    OSTaskCreate((OS_TCB      *)&_APP_Tasks_TCB,
+                 (CPU_CHAR    *)"APP_Tasks",
+                 (OS_TASK_PTR  )_APP_Tasks,
+                 (void        *)0,
+                 (OS_PRIO      )1,
+                 (CPU_STK     *)&_APP_TasksStk[0],
+                 (CPU_STK_SIZE )0u,
+                 (CPU_STK_SIZE )512,
+                 (OS_MSG_QTY   )0u,
+                 (OS_TICK      )0u,
+                 (void        *)0,
+                 (OS_OPT       )(OS_OPT_TASK_NONE | OS_OPT_TASK_STK_CHK | OS_OPT_TASK_STK_CLR),
+                 (OS_ERR      *)&os_err);
+
+
+
+    /* Start RTOS Scheduler. */
+        /**************
+     * Start RTOS *
+     **************/
+    OSStart(&os_err); /* This function never returns. */
+
+}
+
+/*******************************************************************************
+ End of File
+ */
+
